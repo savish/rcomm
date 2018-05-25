8890a99c064e24b20e17c4020df9c5b2118d6ff5
Alan K <afksavish@gmail.com>
2018-05-25 18:24:16 +0200

    Refactor to use an `Iterator`
    Refactored the `while` loop into an `Iterator` that yields the next line
    based on the contents of the two input files
    
    [regression] The `ignore_case` parameter doesn't work anymore
    
    See #2

--- src/main.rs
+++ src/main.rs
@@ -3,47 +3,120 @@ extern crate clap;
 use clap::App;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::{self, BufReader, Write};
+use std::io::{self, BufReader, Lines, Write};
 
-#[derive(Debug)]
-struct Row<'a> {
-    text: &'a str,
-    column: u8,
+// File input to the comm utility
+struct CommFile {
+    lines: Lines<BufReader<File>>,
+    current_line: Option<Result<String, io::Error>>,
+    advance_cursor: bool,
 }
 
-fn display_row(row: &Row) -> String {
-    format!("{}{}", "\t".repeat(row.column as usize), row.text)
-}
-
-fn generate_row<'a>(str1: &'a str, str2: &'a str, ignore_case: bool) -> Row<'a> {
-    let _str1 = if ignore_case {
-        str1.to_lowercase()
-    } else {
-        str1.to_string()
-    };
-
-    let _str2 = if ignore_case {
-        str2.to_lowercase()
-    } else {
-        str2.to_string()
-    };
-
-    if _str1 < _str2 {
-        Row {
-            text: str1,
-            column: 0,
-        }
-    } else if _str1 > _str2 {
-        Row {
-            text: str2,
-            column: 1,
-        }
-    } else {
-        Row {
-            text: str1,
-            column: 2,
+impl CommFile {
+    pub fn new(lines: Lines<BufReader<File>>) -> CommFile {
+        CommFile {
+            lines,
+            current_line: None,
+            advance_cursor: true,
         }
     }
+}
+
+/// Double-file iterator
+struct CommFiles {
+    file1: CommFile,
+    file2: CommFile,
+}
+
+impl CommFiles {
+    pub fn new(file1: Lines<BufReader<File>>, file2: Lines<BufReader<File>>) -> CommFiles {
+        CommFiles {
+            file1: CommFile::new(file1),
+            file2: CommFile::new(file2),
+        }
+    }
+
+    pub fn should_advance_cursor(
+        line1: &Option<String>,
+        line2: &Option<String>,
+    ) -> (bool, bool, u8) {
+        match (line1, line2) {
+            (Some(line1_string), Some(line2_string)) => {
+                if line1_string < line2_string {
+                    (true, false, 0)
+                } else if line1_string > line2_string {
+                    (false, true, 1)
+                } else {
+                    (true, true, 2)
+                }
+            }
+            (Some(_), None) => (true, false, 0),
+            (None, Some(_)) => (false, true, 1),
+            (None, None) => (false, false, 0),
+        }
+    }
+}
+
+impl Iterator for CommFiles {
+    type Item = (String, u8);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Read the next line from each buffer
+        if self.file1.advance_cursor {
+            self.file1.current_line = self.file1.lines.next()
+        };
+
+        if self.file2.advance_cursor {
+            self.file2.current_line = self.file2.lines.next()
+        };
+
+        let file1_next = &self.file1.current_line;
+        let file2_next = &self.file2.current_line;
+
+        if file1_next.is_none() && file2_next.is_none() {
+            // End the iterator when both files have been read to completion
+            None
+        } else {
+            // Gets the next line from the first file, if it exists
+            let file1_line = match file1_next {
+                Some(result) => {
+                    let line_content = match result {
+                        Ok(line) => line,
+                        _ => "",
+                    };
+                    Some(line_content.to_string())
+                }
+                _ => None,
+            };
+
+            // Gets the next line from the second file, if it exists
+            let file2_line = match file2_next {
+                Some(result) => {
+                    let line_content = match result {
+                        Ok(line) => line,
+                        _ => "",
+                    };
+                    Some(line_content.to_string())
+                }
+                _ => None,
+            };
+
+            let cursors = CommFiles::should_advance_cursor(&file1_line, &file2_line);
+            self.file1.advance_cursor = cursors.0;
+            self.file2.advance_cursor = cursors.1;
+
+            match cursors {
+                (_, _, 0) => Some((file1_line.unwrap(), 0)),
+                (_, _, 1) => Some((file2_line.unwrap(), 1)),
+                (_, _, 2) => Some((file1_line.unwrap(), 2)),
+                _ => None,
+            }
+        }
+    }
+}
+
+fn display_line(line: (String, u8)) -> String {
+    format!("{}{}", "\t".repeat(line.1 as usize), line.0)
 }
 
 fn main() -> io::Result<()> {
@@ -51,76 +124,14 @@ fn main() -> io::Result<()> {
     let matches = App::from_yaml(yaml).get_matches();
 
     let file1 = File::open(matches.value_of("file1").unwrap())?;
-    let mut file1_reader = BufReader::new(file1).lines();
-
     let file2 = File::open(matches.value_of("file2").unwrap())?;
-    let mut file2_reader = BufReader::new(file2).lines();
 
-    let mut line1 = file1_reader.next().unwrap_or(Ok("".to_string())).unwrap();
-    let mut line2 = file2_reader.next().unwrap_or(Ok("".to_string())).unwrap();
-
+    let files_lines = CommFiles::new(BufReader::new(file1).lines(), BufReader::new(file2).lines());
     let stdout = io::stdout();
-    let mut handle = stdout.lock();
+    let handle = stdout.lock();
 
-    while !line1.is_empty() && !line2.is_empty() {
-        let line1_ref = &(line1.clone());
-        let line2_ref = &(line2.clone());
-        let row = generate_row(line1_ref, line2_ref, matches.is_present("ignore_case"));
-        writeln!(handle, "{}", display_row(&row))?;
-
-        line1 = match row.column {
-            0 | 2 => file1_reader.next().unwrap_or(Ok("".to_string())).unwrap(),
-            _ => line1.clone(),
-        };
-
-        line2 = match row.column {
-            1 | 2 => file2_reader.next().unwrap_or(Ok("".to_string())).unwrap(),
-            _ => line2.clone(),
-        };
-    }
-
-    if !line1.is_empty() {
-        writeln!(
-            handle,
-            "{}",
-            display_row(&Row {
-                text: &line1,
-                column: 0
-            })
-        )?;
-
-        while let Some(val) = file1_reader.next() {
-            writeln!(
-                handle,
-                "{}",
-                display_row(&Row {
-                    text: &(val.unwrap()),
-                    column: 0
-                })
-            )?;
-        }
-    }
-
-    if !line2.is_empty() {
-        writeln!(
-            handle,
-            "{}",
-            display_row(&Row {
-                text: &line2,
-                column: 1
-            })
-        )?;
-
-        while let Some(val) = file2_reader.next() {
-            writeln!(
-                handle,
-                "{}",
-                display_row(&Row {
-                    text: &(val.unwrap()),
-                    column: 1
-                })
-            )?;
-        }
+    for lines in files_lines {
+        writeln!(handle, "{}", display_line(lines))
     }
 
     Ok(())
